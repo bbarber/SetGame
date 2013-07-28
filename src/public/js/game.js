ef666c895055d78bb576c078640f06b39befb9e8
branmantwo <branden.barber1@gmail.com>
2013-07-28 17:21:57 -0500

    Changing ajax calls to work with ruby endpoints

---
+++ src/public/js/game.js
@@ -0,0 +1,159 @@
+setgame.game = (function () {
+    var game = {};
+
+    game.startDailyGame = function (userName) {
+        if (setgame.viewModel.currentTab() === 'practice') {
+            game.start();
+        }
+        else if (userName == null || userName === '') {
+            setgame.viewModel.login(true);
+        }
+        else {
+            setgame.viewModel.login(false);
+            game.start();
+        }
+    }
+
+    game.start = function () {
+
+        var isPractice = setgame.viewModel.currentTab() === 'practice';
+        Math.seedrandom(isPractice ? Date.now() : setgame.viewModel.seed());
+
+        setgame.viewModel.gameState('loading');
+        setgame.viewModel.gameState('started');
+        setgame.engine.initializeBoard();
+        setgame.viewModel.startTime(new Date().getTime());
+    }
+
+    game.getTodaysSeed = function () {
+        // Caclulate offset milliseconds, so we can use local time
+        var offset = new Date().getTimezoneOffset() * 1000 * 60;
+        var today = Math.floor((Date.now() - offset) / 1000 / 60 / 60 / 24);
+        
+        $.ajax({
+            url: 'api/Game/GetDailySeed/' + today,
+            dataType: 'json',
+            success: function (seed) {
+                Math.seedrandom(seed);
+                setgame.viewModel.seed(seed);
+                setgame.viewModel.LeaderboardVM.getAllGames();
+
+                ko.applyBindings(setgame.viewModel);
+
+                $('.demo-3').hide('fast', function () {
+                    $('.container-narrow').show('slow', function () {
+                    });
+                });
+
+
+
+            }
+        });
+    }
+
+    game.checkIfValidSet = function (set) {
+
+        if (setgame.engine.isValidSet(set[0], set[1], set[2])) {
+            if (setgame.game.isUnique(set)) {
+                setgame.viewModel.foundSets.push(set);
+            }
+            else {
+                alert("Already found");
+            }
+        }
+        else {
+            alert('Not valid set');
+        }
+
+
+        if (setgame.viewModel.foundSets().length === 6) {
+            game.winning();
+        }
+
+
+        game.clearSelection();
+    }
+
+    game.winning = function () {
+        var score = (new Date().getTime() - setgame.viewModel.startTime()) / 1000.0;
+        setgame.viewModel.score(score);
+        setgame.viewModel.gameState('winning');
+
+        var wasPractice = setgame.viewModel.currentTab() === 'practice';
+        setgame.viewModel.wasPractice(wasPractice);
+
+        setgame.viewModel.currentTab('stats');
+
+        if (!wasPractice) {
+            console.error("Fix this ajax");
+            $.ajax({
+                url: 'api/Game/Completed?userName=' + setgame.helpers.GetUserName() + "&score=" + score + "&seed=" + setgame.viewModel.seed(),
+                dataType: 'json',
+                success: function () {
+                    getStats(GetUserName());
+                }
+            });
+        }
+    }
+
+    game.isUnique = function (set) {
+        var foundSets = setgame.viewModel.foundSets();
+
+        var fileNames = [];
+        for (var i in foundSets) {
+            fileNames.push($.map(foundSets[i], function (val) {
+                return val.fileName;
+            }).sort());
+        }
+
+        var curSetFileNames = $.map(set, function (val, i) {
+            return val.fileName;
+        }).sort();
+
+
+        for (var i in fileNames) {
+            if (fileNames[i][0] === curSetFileNames[0] &&
+                fileNames[i][1] === curSetFileNames[1] &&
+                fileNames[i][2] === curSetFileNames[2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    game.practiceMeow = function () {
+        setgame.viewModel.currentTab('practice');
+        setgame.viewModel.board([]);
+        setgame.viewModel.foundSets([]);
+        setgame.game.start();
+    }
+
+    game.clearSelection = function () {
+
+        setgame.viewModel.selections = ko.observableArray();
+
+        for (var i = 0; i < setgame.viewModel.board().length; i++) {
+            for (var j = 0; j < setgame.viewModel.board()[i].length; j++) {
+                setgame.viewModel.board()[i][j].isSelected(false);
+            }
+        }
+
+    }
+
+    game.getStats = function (userName) {
+        console.error("Fix this ajax");
+        $.ajax({
+            url: 'api/Game/GetAllGames',
+            dataType: 'json',
+            success: function (games) {
+                setgame.viewModel.allGames(games);
+            }
+        });
+    }
+
+    return game;
+}());
+
+
