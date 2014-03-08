2dcabe63d474b1f9cec7604f9eadac0906b6557e
bbarber <bbarber@nebraskaglobal.com>
2014-03-08 17:59:07 -0600

    Zomg, first unit test

---
+++ node/test/engine.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('engine', function() {
+    describe('testme', function() {
+        it('should pass', function(){
+            assert.equal('Thanks Chad!', 'Thanks Chad!');    
+        })        
+    })
+})
