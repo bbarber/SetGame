76c44b641d9e37596c5e603ed1f94aaa3f10a4ca
Branden <bbarber@nebraskaglobal.com>
2014-06-22 11:11:19 -0500

    Update startup script so it ignore session hangups

--- misc/start.sh
+++ misc/start.sh
@@ -13,8 +13,8 @@ do
         lastBackup=$curDate
 
         # Starts mongodb and node
-        mongod --dbpath ~/setgame/data/ --rest --port 27017 > ~/setgame/misc/logs/mongod.log &
-        node ~/setgame/app.js > ~/setgame/misc/logs/node.log &
+        nohup mongod --dbpath ~/setgame/data/ --rest --port 27017 > ~/setgame/misc/logs/mongod.log &
+        nohup node ~/setgame/app.js > ~/setgame/misc/logs/node.log &
 
     fi
     sleep 600
