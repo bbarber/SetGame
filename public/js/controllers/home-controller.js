0dc9c07c2830f2aacb25e3ec728df17ba3873f64
branmantwo <branden.barber1@gmail.com>
2014-07-12 23:11:43 -0500

    Play/Practice meow button, refactored header tab

--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-setgame.controller('HomeController', ['$scope',
-    function($scope){
-        $scope.greeting = "hello";    
+setgame.controller('HomeController', ['$scope', '$location',
+    function ($scope, $location) {
+        $scope.isFoRealz = $location.path().toLowerCase() !== '/practice';
     }
 ]);
--- public/js/directives/header.js
+++ public/js/directives/header.js
@@ -1,35 +1,14 @@
 'use strict';
 
-setgame.directive('sgHeader', function() {
+setgame.directive('sgHeader', function () {
     return {
         restrict: 'E',
         templateUrl: 'html/header.html',
         replace: true,
-        controller: ['$scope', '$location', function($scope, $location) {
-
-            // Select home on page load
-            $scope.isHome = true;
-
-
-            $scope.$on('$locationChangeStart', function () {
-                var path = $location.path();
-                
-                $scope.isHome = false;
-                $scope.isPractice = false;
-                $scope.isStats = false;
-                $scope.isLeaderboard = false;
-
-                if(path === "/")
-                    $scope.isHome = true;
-                else if(path === "/practice")
-                    $scope.isPractice = true;
-                else if(path === "/stats")
-                    $scope.isStats = true;
-                else if(path === "/leaderboard")
-                    $scope.isLeaderboard = true;
-            });
-
-
+        controller: ['$scope', '$location', function ($scope, $location) {
+            $scope.isCurrentPath = function (path) {
+                return $location.path().toLowerCase() === path.toLowerCase();
+            };
         }]
     };
 });
