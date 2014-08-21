59a3167f2735d55c63bb5396effdc0d682c5e42d
bbarber <bbarber@nebraskaglobal.com>
2014-08-20 22:11:01 -0500

    Boards now contain 6 valid sets, still need tests

--- public/js/services/engine.js
+++ public/js/services/engine.js
@@ -3,13 +3,14 @@
 setgame.factory('engine', ['deck', 'card', function(deck, card) {
 
     return {
-        createBoard: createBoard
+        createBoard: createBoard,
+        isValidSet: isValidSet
     };
 
     function createBoard() {
         do {
             var board = getBoard();
-            var numSets = getSets();
+            var numSets = getSets(board);
         } while(numSets !== 6);
 
         return board;
@@ -35,8 +36,50 @@ setgame.factory('engine', ['deck', 'card', function(deck, card) {
         return board;
     }
 
-    function getSets() {
-        return 6;
+    function getSets(board) {
+        var numSets = 0;
+
+        for(var i = 0; i < 10; i++) {
+            for(var j = 1 + i; j < 11; j++) {
+                for(var k = 1 + j; k < 12; k++) {
+                    if(isValidSet([board[i], board[j], board[k]]))
+                        numSets++;
+                }
+            }
+        }
+
+        return numSets;
     }
+
+    function isValidSet(cards) {
+        for(var attr in card) {
+
+            // Look at each attribute, and see if they're
+            // all the same, or all different
+            var values = [
+                cards[0][attr],
+                cards[1][attr],
+                cards[2][attr]
+            ];
+
+            if(!hasSame(values) && !hasDifferent(values))
+                return false;
+        }
+
+        // If we got here, it's a valid set
+        return true;
+    }
+
+    function hasSame(values) {
+        return values[0] === values[1]
+            && values[0] === values[2];
+    }
+
+    function hasDifferent(values) {
+        return values[0] !== values[1]
+            && values[0] !== values[2]
+            && values[1] !== values[2];
+    }
+
 
 }]);
