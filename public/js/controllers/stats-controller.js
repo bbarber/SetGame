fad079477c44374fc7b598be2aa40345d52b29e9
bbarber <bbarber@nebraskaglobal.com>
2014-11-02 15:15:24 -0600

    Hooked up the leaderboard, added a filter for formatting dates/scores

--- public/js/controllers/stats-controller.js
+++ public/js/controllers/stats-controller.js
@@ -2,12 +2,9 @@
 
 setgame.controller('StatsController', ['$scope', '$rootScope', 'common',
   function($scope, $rootScope, common) {
-    $scope.minutes = parseInt($rootScope.score / 60, 10);
-    $scope.seconds = parseInt($rootScope.score % 60, 10);
-    $scope.playAgain = function() {
 
-    };
-
+  
+    $scope.score = $rootScope.score;
     $scope.isPractice = common.isPractice();
   }
 ]);
