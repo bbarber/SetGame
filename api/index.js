d88a52d130292f1e07ed1e88734b8ab3fa673a95
bbarber <bbarber@nebraskaglobal.com>
2017-01-01 14:00:30 -0600

    Restore complete code

--- api/index.js
+++ api/index.js
@@ -9,6 +9,41 @@ module.exports.set = function (app, games, http, secrets) {
         });
     });
 
+    app.get('/api/Completed/:username/:score/:seed', function (req, res) {
+
+        var game = {
+            Seed: req.params.seed,
+            UserName: req.params.username
+        };
+
+        games.find(game).toArray(function (err, doc) {
+
+            // Check for duplicates
+            if (doc.length === 0) {
+
+                game.DatePlayed = new Date();
+                game.Score = req.params.score;               
+              
+                games.insert(game, function (err, doc) {
+                    
+                    // Slackbot message *after* we insert the record
+                    var params = '?token=' + secrets.slack.token + '&channel=%23' + secrets.slack.channel;
+                    var options = {
+                        url: 'https://nebraskaglobal.slack.com/services/hooks/slackbot' + params,
+                        body: '*SetGame*  `' + game.UserName + '` - `' + game.Score + 's`'
+                    };                
+                    request.post(options);
+                    
+                    res.send(doc);
+                });
+            }
+            else {
+                console.log('duplicate game, not saving...');
+                res.send('duplicate game');
+            }
+        });
+    });
+
     app.get('/api/GetLeaderboard/:seed', function (req, res) {
         games.find({}).toArray(function (err, doc) {
 
